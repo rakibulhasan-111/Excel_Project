90112bdbab8e20ebc9c953662119589d98378930
rakibulhasan-111 <rhs.sft.111@gmail.com>
2023-03-23 18:43:13 +0600

    add number

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -4,6 +4,8 @@ namespace App\Imports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class UsersImport implements ToModel
 {
@@ -14,9 +16,16 @@ class UsersImport implements ToModel
     */
     public function model(array $row)
     {
+        $uniqueNumber = mt_rand(1000, 9999);
+
+        $lastId = DB::table('users')->latest()->value('id');
+        
+        $lastId = $lastId+1;
+        $email = 'user_'.$lastId.'@example.com';
         return new User([
             'name'=>$row[0],
-            'email'=>$row[1],
+            'number'=>$row[1],
+            'email'=>$email,
             'password'=>bcrypt($row[2])
         ]);
     }
